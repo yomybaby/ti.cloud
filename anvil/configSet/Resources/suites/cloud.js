c73a66ed1ff16b2bfec66321f1f228add32f0e6f
Jeff English <jeff@utmostlogic.com>
2012-07-16 16:58:03 -0500

    [MOD-655] Create initial users for anvil tests

--- anvil/configSet/Resources/suites/cloud.js
+++ anvil/configSet/Resources/suites/cloud.js
@@ -18,7 +18,8 @@ module.exports = new function() {
     this.name = "cloud";
     this.tests = [
         {name: "cloudModule"},
-        {name: "cloudUseSecure"}
+        {name: "cloudUseSecure"},
+	    {name: "cloudEnsureSetup"}
     ];
 
     // ---------------------------------------------------------------
@@ -44,4 +45,62 @@ module.exports = new function() {
         valueOf(testRun, Cloud.useSecure).shouldBeTrue();
         finish(testRun);
     };
+
+	this.cloudEnsureSetup = function(testRun) {
+		var loggedOut2 = function(e) {
+			valueOf(testRun, e.success).shouldBeTrue();
+			finish(testRun);
+		};
+
+		var created2 = function(e) {
+            valueOf(testRun, e.success).shouldBeTrue();
+            valueOf(testRun, e.error).shouldBeFalse();
+            Cloud.Users.logout(loggedOut2);
+        };
+
+		var login2 = function(e) {
+			if (e.success == false) {
+				var data = {
+                    username: 'chatuser',
+                    password: 'password',
+                    password_confirmation: 'password'
+                };
+				Cloud.Users.create(data, created1);
+			} else {
+				created2(e);
+			}
+		};
+
+		var loggedOut1 = function(e) {
+			valueOf(testRun, e.success).shouldBeTrue();
+			Cloud.Users.login({
+				login: 'chatuser',
+				password: 'password'
+			}, login2);
+		};
+
+		var created1 = function(e) {
+            valueOf(testRun, e.success).shouldBeTrue();
+            valueOf(testRun, e.error).shouldBeFalse();
+            Cloud.Users.logout(loggedOut1);
+        };
+
+		var login1 = function(e) {
+			if (e.success == false) {
+				var data = {
+                    username: 'drillbituser',
+                    password: 'password',
+                    password_confirmation: 'password'
+                };
+				Cloud.Users.create(data, created1);
+			} else {
+				created1(e);
+			}
+		};
+
+		Cloud.Users.login({
+			login: 'drillbituser',
+			password: 'password'
+		}, login1);
+	}
 }
