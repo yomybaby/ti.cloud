f84ddb842329b5f3ffc82e01d57380d0fff511f4
Tim Statler <tstatler@appcelerator.com>
2014-04-14 15:36:08 -0700

    [TIMOOPEN-383]Updated sample app to send latitude/longitude with updateSubscription() call.

--- commonjs/example/windows/pushNotifications/updateSubscription.js
+++ commonjs/example/windows/pushNotifications/updateSubscription.js
@@ -29,14 +29,45 @@ windowFunctions['Update Subscription'] = function (evt) {
     });
     content.add(channel);
 
+    // These variables represent the device's geographic coordinates,
+    // and are passed in the 'loc' parameter to the updateSubscription() method.
+    // This enables a Dashboard administrator to send a push notification to this user based on their location.
+
+    var latitude = 37.3895100;
+    var longitude = -122.0502150;
+
+    var latitude = Ti.UI.createTextField({
+        hintText: 'Latitude',
+        top: 10 + u, left: 10 + u, right: 10 + u,
+        height: 40 + u,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+        autocorrect: false,
+        value: latitude
+    });
+    content.add(latitude);
+    
+    var longitude = Ti.UI.createTextField({
+        hintText: 'Longitude',
+        top: 10 + u, left: 10 + u, right: 10 + u,
+        height: 40 + u,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        autocapitalization: Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+        autocorrect: false,
+        value: longitude
+    });
+    content.add(longitude);
+
     var button = Ti.UI.createButton({
         title: 'Update',
         top: 10 + u, left: 10 + u, right: 10 + u, bottom: 10 + u,
         height: 40 + u
     });
-    content.add(button);
+    content.add(button);    
 
     var fields = [ channel ];
+
+
 
     function submitForm() {
         for (var i = 0; i < fields.length; i++) {
@@ -50,7 +81,8 @@ windowFunctions['Update Subscription'] = function (evt) {
 
         Cloud.PushNotifications.updateSubscription({
             channel: channel.value,
-            device_token: pushDeviceToken
+            device_token: pushDeviceToken,
+            loc: [parseInt(longitude.value), parseInt(latitude.value)]
         }, function (e) {
             if (e.success) {
                 channel.value = '';
